bb7c5853284268b6f9d1d443f029a0f0f0b8e1fa
graudinsgg <112856640+graudinsgg@users.noreply.github.com>
2024-04-15 15:40:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert(1)
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu()darbojas');
+    let zina = document.querySelector('zina').value;
+    let zinas = document.querySelector('CataZina');
+    zinas.innerHTML = zinas.innerHTML+ '<br>'+zina.value;
+
+}
